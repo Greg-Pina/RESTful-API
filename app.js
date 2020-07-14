cdf198331d5d568e2f30db64f0bc438d6e09091d
Greg Pina <pinagm2825@gmail.com>
2020-07-14 13:29:04 -0400

    finished CRUD

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const connectDB = require('./config/db')
+const bodyParser = require('body-parser')
 const app = express()
 
 // Load config
 dotenv.config({ path: './config/config.env' })
 connectDB()
+
+// Middleware body-parser
+app.use(bodyParser.json())
 
 //Import Routes
 const postsRoute = require('./routes/posts')
--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,61 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-	res.send('We are on posts')
+// Gets all posts
+router.get('/', async (req, res) => {
+	try {
+		const posts = await Post.find()
+		res.json(posts)
+	} catch (err) {
+		res.json({ message: err })
+	}
 })
 
-router.post('/', (req, res) => {
-	const posts = req.body
+// submits a post
+router.post('/', async (req, res) => {
+	const newPosts = new Post({
+		title: req.body.title,
+		description: req.body.description,
+	})
+	try {
+		const savedPost = await newPosts.save()
+		res.json(savedPost)
+	} catch (err) {
+		res.json({ message: err })
+	}
+})
+
+// Gets specific posts
+router.get('/:postId', async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.postId)
+		res.json(post)
+	} catch (err) {
+		res.json({ message: err })
+	}
+})
+
+// Update a post
+router.patch('/postId', async (req, res) => {
+	try {
+		const updatePost = await Post.updateOne(
+			{ _id: req.params.postId },
+			{ $set: { title: req.body.title } }
+		)
+		res.json(updatePost)
+	} catch (err) {
+		res.json({ message: err })
+	}
+})
+
+// Delete specific post
+router.delete('/postId', async (req, res) => {
+	try {
+		const deletePost = await Post.remove({ _id: req.params.postId })
+		res.json(deletePost)
+	} catch (err) {
+		res.json({ message: err })
+	}
 })
 
 module.exports = router
